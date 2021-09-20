42525e1b32d503016192df159f346d13e635d222
Lâm Minh Thiện - 60136962 <minhthienmap@gmail.com>
2021-09-19 21:01:16 -0700

    Sửa lỗi bài 2

--- LAB_2/bai2.php
+++ LAB_2/bai2.php
@@ -3,9 +3,9 @@
 	$a=rand(1,10);
 	echo "Số ngẫu nhiên  có giá trị trong khoảng [10,1000] là ".$a.'<br>' ;
 
-	echo 'Bảng cửu chương'.$a.'là <br>';
+	echo 'Bảng cửu chương '.$a.' là <br>';
 
-	for ($i=1; $i < a ; $i++) { 
+	for ($i=1; $i <= 10 ; $i++) { 
 		echo $a.'*'.$i.'='.$a*$i.'<br>';
 	}
 
