97048363e3544705d28104748f57bc74185ccc0b
Lâm Minh Thiện - 60136962 <minhthienmap@gmail.com>
2021-09-20 07:32:06 -0700

    hoàn chỉnh bài 4

--- LAB_2/bai4.php
+++ LAB_2/bai4.php
@@ -1,16 +1,36 @@
-<?php
 
+
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+
+
+<?php
 $m = rand(2,5);
 $n = rand(2,5);
 
+	echo "<h2>Ma trận $m x $n ngẫu nhiên</h2>";
+	echo "<table style='width:100%'>";
 	for ($j=1; $j<=$m; $j++)
 		{
-
+			echo "<tr>";
 			for ($i=1; $i<=$n; $i++) 
 			{ 
-				echo rand(-100,100).' | ';
+				$a=rand(-100,100);
+				// echo rand(-100,100).' | ';
+				echo "<td> $a </td>";
 			}
-			echo '<br>';
+			echo "</tr>";	
 		}
+	echo "</table>";
 
 ?>
+
+
+</body>
+</html>
