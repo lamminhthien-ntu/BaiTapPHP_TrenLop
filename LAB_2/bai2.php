75e11eaeb6f79b6a97e8398f33bd5ab022dd133e
Lâm Minh Thiện - 60136962 <minhthienmap@gmail.com>
2021-09-19 20:58:07 -0700

    Create bai2.php

---
+++ LAB_2/bai2.php
@@ -0,0 +1,16 @@
+<?php
+	//Tạo ngẫu nhiên 1 số nguyên có giá trị trong khoảng [1,10]. 
+	$a=rand(1,10);
+	echo "Số ngẫu nhiên  có giá trị trong khoảng [10,1000] là ".$a.'<br>' ;
+
+	echo 'Bảng cửu chương'.$a.'là <br>';
+
+	for ($i=1; $i < a ; $i++) { 
+		echo $a.'*'.$i.'='.$a*$i.'<br>';
+	}
+
+
+
+
+
+?>
