22bfd1528ece845245ea8c3f7cce0f402f0a1551
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-10-11 08:53:32 +0700

    .

--- LAB_4/VỪA TRỪU TƯỢNG VỪA KẾ THỪA.php
+++ LAB_4/VỪA TRỪU TƯỢNG VỪA KẾ THỪA.php
@@ -1,23 +1,17 @@
 <?php
-
 interface water{
     public function makeItWet();
 }
-
 class weather{
     public function start(){
         return 'Here is some weather';
     }
 }
-
 class rain extends weather implements water{
     public function makeItWet(){
         return 'It is wet';
     }
 }
-
-
-
 $a = new rain();
 echo $a->start();
 echo $a->makeItWet()
